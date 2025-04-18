b72c2d6ea39476d46fcdd6dea0ef1d37a5a1a2b2
VodVas <142312712+VodVas@users.noreply.github.com>
2025-04-19 00:43:33 +0400

    add name

--- Editor/InterfaceReferenceDrawer.cs
+++ Editor/InterfaceReferenceDrawer.cs
@@ -2,47 +2,50 @@
 using UnityEditor;
 using UnityEngine;
 using System;
-using VodVas.InterfaceSerializer;
 
-[CustomPropertyDrawer(typeof(InterfaceConstraintAttribute))]
-public class InterfaceReferenceDrawer : PropertyDrawer
+namespace VodVas.InterfaceSerializer
 {
-    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+
+    [CustomPropertyDrawer(typeof(InterfaceConstraintAttribute))]
+    public class InterfaceReferenceDrawer : PropertyDrawer
     {
-        var constraint = (InterfaceConstraintAttribute)attribute;
-        EditorGUI.BeginProperty(position, label, property);
-
-        MonoBehaviour current = property.objectReferenceValue as MonoBehaviour;
-        MonoBehaviour selected = EditorGUI.ObjectField(
-            position,
-            label,
-            current,
-            typeof(MonoBehaviour),
-            true) as MonoBehaviour;
-
-        ValidateSelection(selected, constraint.InterfaceType, property);
-
-        EditorGUI.EndProperty();
-    }
-
-    private void ValidateSelection(MonoBehaviour selected, Type interfaceType, SerializedProperty property)
-    {
-        if (selected == null)
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
-            property.objectReferenceValue = null;
-            return;
+            var constraint = (InterfaceConstraintAttribute)attribute;
+            EditorGUI.BeginProperty(position, label, property);
+
+            MonoBehaviour current = property.objectReferenceValue as MonoBehaviour;
+            MonoBehaviour selected = EditorGUI.ObjectField(
+                position,
+                label,
+                current,
+                typeof(MonoBehaviour),
+                true) as MonoBehaviour;
+
+            ValidateSelection(selected, constraint.InterfaceType, property);
+
+            EditorGUI.EndProperty();
         }
 
-        bool isValid = interfaceType.IsAssignableFrom(selected.GetType());
-
-        if (!isValid)
+        private void ValidateSelection(MonoBehaviour selected, Type interfaceType, SerializedProperty property)
         {
-            Debug.LogError($"{selected.name} must implement {interfaceType.Name} interface!");
-            property.objectReferenceValue = null;
-            return;
-        }
+            if (selected == null)
+            {
+                property.objectReferenceValue = null;
+                return;
+            }
 
-        property.objectReferenceValue = selected;
+            bool isValid = interfaceType.IsAssignableFrom(selected.GetType());
+
+            if (!isValid)
+            {
+                Debug.LogError($"{selected.name} must implement {interfaceType.Name} interface!");
+                property.objectReferenceValue = null;
+                return;
+            }
+
+            property.objectReferenceValue = selected;
+        }
     }
 }
 #endif
